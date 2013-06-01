142a54144c5c3514c16b6f5e7510f227d650dd9d
Martin Stone Davis <martin.stone.davis@gmail.com>
2013-05-31 22:06:33 -0700

    add parsing of a premise

--- Oscar/Problem.hs
+++ Oscar/Problem.hs
@@ -4,6 +4,8 @@ module Oscar.Problem where
 import Text.Parsec
 import Data.Char
 import Data.String
+import Data.Ratio
+import Numeric
 
 newtype
     ProblemNumber
@@ -17,11 +19,22 @@ newtype
     String
   deriving Show
 
+type Key = String
+type JustificationType = Rational  
+
+data 
+    Premise
+  = Premise
+      Key
+      Rational
+  deriving Show
+
 data
     Problem
   = Problem
-    ProblemNumber
-    ProblemDescription
+      ProblemNumber
+      ProblemDescription
+      [Premise]
   deriving Show
 
 problemParser :: Stream s m Char => ParsecT s u m Problem
@@ -30,11 +43,18 @@ problemParser
     string "Problem #"
     n <- many1 digit
     newline
-    s <- many anyChar
-    return (Problem (ProblemNumber (read n)) (ProblemDescription s))
+    s <- manyTill anyChar $ try newline
+    string "Given premises:"
+    newline
+    many space
+    k <- many1 alphaNum
+    many space
+    string "justification = "
+    j <- manyTill anyChar $ try newline
+    return (Problem (ProblemNumber (read n)) (ProblemDescription s) [Premise k (fst . head $ readFloat j)])
 
 main :: IO ()
 main = do
-  putStrLn . show $ Problem (ProblemNumber 1) (ProblemDescription "hello world")
-  putStrLn . show $ Problem (ProblemNumber 2) (ProblemDescription "goodbye world")
-  parseTest problemParser "Problem #42\nHello, World!"
+  putStrLn . show $ Problem (ProblemNumber 1) (ProblemDescription "hello world") [Premise "p1" 1]
+  putStrLn . show $ Problem (ProblemNumber 2) (ProblemDescription "goodbye world") [Premise "p1" 1, Premise "p2" (1 % 2)]
+  parseTest problemParser "Problem #1\nThis is a case of collective rebutting defeat\nGiven premises:\n     P    justification = 1.0\n     A    justification = 1.0\nUltimate epistemic interests:\n     R    interest = 1.0\n    FORWARDS PRIMA FACIE REASONS\n      pf-reason_1:   {P} ||=> Q   strength = 1.0\n      pf-reason_2:   {Q} ||=> R   strength = 1.0\n      pf-reason_3:   {C} ||=> ~R   strength = 1.0\n      pf-reason_4:   {B} ||=> C   strength = 1.0\n      pf-reason_5:   {A} ||=> B   strength = 1.0\n"
