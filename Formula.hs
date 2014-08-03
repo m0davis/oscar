2f2d2843e44067c4f9d35965d5dfb288ce10ac03
Martin Stone Davis <martin.stone.davis@gmail.com>
2014-08-03 06:12:50 -0700

    add PQToken module

--- Formula.hs
+++ Formula.hs
@@ -8,87 +8,26 @@
 module Formula where
 
 import ClassyPrelude hiding (
-    Text, 
+    --Text, 
     try,
     )
 
-import Control.Applicative          (empty)
-import Control.Applicative          (many)
 import Control.Monad.Free           (Free(Free))
 import Control.Monad.Free           (Free(Pure))
-import Data.Text                    (Text)
+--import Data.Text                    (Text)
 --import Data.Text.Internal.Lazy      (Text)
-import Text.Parsec.Char             (alphaNum)
-import Text.Parsec.Char             (char)
-import Text.Parsec.Char             (space)
-import Text.Parsec.Char             (string)
-import Text.Parsec.Combinator       (many1)
-import Text.Parsec.Prim             (try)
---import Text.Parsec.Text.Lazy        (Parser)
-import Text.Parsec.Text             (Parser)
 import Text.Show.Pretty             (ppShow)
 
-import Utilities                    (simpleParse)
 import Parenthesis                  (freeFromParentheses)
-import Parenthesis                  (Parenthesis(OpenParenthesis))
-import Parenthesis                  (Parenthesis(CloseParenthesis))
-import QUBS                         (Quantifier(Universal))
-import QUBS                         (Quantifier(Existential))
-import QUBS                         (UnaryOp(Negation))
-import QUBS                         (UnaryOp(Whether))
-import QUBS                         (BinaryOp(Conjunction))
-import QUBS                         (BinaryOp(Disjunction))
-import QUBS                         (BinaryOp(Conditional))
-import QUBS                         (BinaryOp(Biconditional))
-import QUBS                         (BinaryOp(Defeater))
-import QUBS                         (Symbol(Symbol))
+import QUBS                         (Quantifier)
+import QUBS                         (UnaryOp)
+import QUBS                         (BinaryOp)
+import QUBS                         (Symbol)
 import QToken                       (QToken(QTokenUnaryOp))
 import QToken                       (QToken(QTokenBinaryOp))
 import QToken                       (QToken(QTokenQuantifier))
 import QToken                       (QToken(QTokenSymbol))
-
--- PQToken
--- import Parenthesis (Parenthesis)
--- import QToken
--- import QUBS
-type PQToken = Either Parenthesis QToken
-
-makePQTokens :: Text -> [PQToken]
-makePQTokens = simpleParse $ many $ many space *> parsePQToken
-  where
-    parsePQToken :: Parser PQToken
-    parsePQToken = empty
-        <|> Left                     <$> parenthesis
-        <|> Right . QTokenUnaryOp    <$> unaryOp
-        <|> Right . QTokenBinaryOp   <$> binaryOp
-        <|> Right . QTokenQuantifier <$> quantifier
-        <|> Right . QTokenSymbol     <$> symbol
-      where
-        p **> v = try p *> pure v
-        infixl 4 **>
-
-        parenthesis = empty
-            <|> char '('            **> OpenParenthesis
-            <|> char '['            **> OpenParenthesis
-            <|> char ')'            **> CloseParenthesis
-            <|> char ']'            **> CloseParenthesis
-
-        unaryOp = empty
-            <|> char '?'            **> Whether
-            <|> char '~'            **> Negation
-
-        binaryOp = empty
-            <|> char 'v' *> space   **> Disjunction
-            <|> char '&'            **> Conjunction
-            <|> char '@'            **> Defeater
-            <|> string "->"         **> Conditional
-            <|> string "<->"        **> Biconditional
-
-        quantifier = empty
-            <|> string "all"        **> Universal
-            <|> string "some"       **> Existential
-
-        symbol = Symbol . pack <$> many1 alphaNum
+import PQToken                      (makePQTokens)
 
 -- QSToken
 -- import QUBS
---
+++ PQToken.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module PQToken where
+
+import ClassyPrelude hiding (
+    Text, 
+    try,
+    )
+
+import Control.Applicative          (empty)
+import Control.Applicative          (many)
+import Data.Text                    (Text)
+import Text.Parsec.Char             (alphaNum)
+import Text.Parsec.Char             (char)
+import Text.Parsec.Char             (space)
+import Text.Parsec.Char             (string)
+import Text.Parsec.Combinator       (many1)
+import Text.Parsec.Prim             (try)
+--import Text.Parsec.Text.Lazy        (Parser)
+import Text.Parsec.Text             (Parser)
+
+import Parenthesis                  (Parenthesis(OpenParenthesis))
+import Parenthesis                  (Parenthesis(CloseParenthesis))
+import QUBS                         (Quantifier(Universal))
+import QUBS                         (Quantifier(Existential))
+import QUBS                         (UnaryOp(Negation))
+import QUBS                         (UnaryOp(Whether))
+import QUBS                         (BinaryOp(Conjunction))
+import QUBS                         (BinaryOp(Disjunction))
+import QUBS                         (BinaryOp(Conditional))
+import QUBS                         (BinaryOp(Biconditional))
+import QUBS                         (BinaryOp(Defeater))
+import QUBS                         (Symbol(Symbol))
+import QToken                       (QToken(QTokenUnaryOp))
+import QToken                       (QToken(QTokenBinaryOp))
+import QToken                       (QToken(QTokenQuantifier))
+import QToken                       (QToken(QTokenSymbol))
+import Utilities                    (simpleParse)
+
+type PQToken = Either Parenthesis QToken
+
+makePQTokens :: Text -> [PQToken]
+makePQTokens = simpleParse $ many $ many space *> parsePQToken
+  where
+    parsePQToken :: Parser PQToken
+    parsePQToken = empty
+        <|> Left                     <$> parenthesis
+        <|> Right . QTokenUnaryOp    <$> unaryOp
+        <|> Right . QTokenBinaryOp   <$> binaryOp
+        <|> Right . QTokenQuantifier <$> quantifier
+        <|> Right . QTokenSymbol     <$> symbol
+      where
+        p **> v = try p *> pure v
+        infixl 4 **>
+
+        parenthesis = empty
+            <|> char '('            **> OpenParenthesis
+            <|> char '['            **> OpenParenthesis
+            <|> char ')'            **> CloseParenthesis
+            <|> char ']'            **> CloseParenthesis
+
+        unaryOp = empty
+            <|> char '?'            **> Whether
+            <|> char '~'            **> Negation
+
+        binaryOp = empty
+            <|> char 'v' *> space   **> Disjunction
+            <|> char '&'            **> Conjunction
+            <|> char '@'            **> Defeater
+            <|> string "->"         **> Conditional
+            <|> string "<->"        **> Biconditional
+
+        quantifier = empty
+            <|> string "all"        **> Universal
+            <|> string "some"       **> Existential
+
+        symbol = Symbol . pack <$> many1 alphaNum
