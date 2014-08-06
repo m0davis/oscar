851c452ebe42a02c6b2ac11b0b84eb21edb5fffa
Martin Stone Davis <martin.stone.davis@gmail.com>
2014-08-06 10:01:42 -0700

    remove unnecessary type declaration; whitespace

--- src/Oscar/Problem.hs
+++ src/Oscar/Problem.hs
@@ -313,16 +313,16 @@ enbracedListParser = do
             return [firstText]
 
 reasonBlocks ∷ forall direction defeasible.
-    (Text ::: ƮSection (Reasons (direction ∷ Direction) (defeasible ∷ Defeasibility))) →
-    [ReasonBlock (direction ∷ Direction) (defeasible ∷ Defeasibility)]
+    Text ::: ƮSection (Reasons direction defeasible) →
+    [ReasonBlock direction defeasible]
 reasonBlocks = simpleParse (many (try p) <* many space <* eof) . unƭ
   where
-    p ∷ Parser (ReasonBlock (direction ∷ Direction) (defeasible ∷ Defeasibility))
+    p ∷ Parser (ReasonBlock direction defeasible)
     p = do
         n ← parserProblemReasonName
         spaces
         (t, (v, d)) ← many anyChar `precededBy` p'
-        return $ ReasonBlock n (coerce  . (pack ∷ String → Text) $ t) v d
+        return $ ReasonBlock n (coerce . (pack ∷ String → Text) $ t) v d
       where
         p' ∷ Parser (Text ::: ƮProblemVariables, ProblemStrengthDegree)
         p' = do
