e3185bca50fb3e8e8d4eb246691c05cf0f5c5f16
Martin Stone Davis <martin.stone.davis@gmail.com>
2016-11-16 21:33:52 -0800

    make simulate-oscar not clobber the *use-reductio* variable

--- lisp-sbcl/monolithic-oscar.lisp
+++ lisp-sbcl/monolithic-oscar.lisp
@@ -17939,7 +17939,6 @@ Ultimate epistemic interests:
   (let ((problem (assoc n *simulation-problems*)))
     (when (null problem)
       (princ "There is no problem of this number.") (terpri) (return-from simulate-oscar))
-    (setf *use-reductio* reductio?)
     (setf *problem-number* (mem1 problem))
     (setf *msg* (mem2 problem))
     (setf *start-time* (mem3 problem))
@@ -17948,8 +17947,8 @@ Ultimate epistemic interests:
     (setf *inputs* (mem6 problem))
     (setf **premises** (mem7 problem))
     (setf *substantive-interests* (mem8 problem))
-    (let ((pp *print-pretty*))
-      (setf *print-pretty* nil)
+    (let ((*use-reductio* reductio?)
+          (*print-pretty* nil))
       (display-settings)
       (setf *premises* **premises**)
       (initialize-reasoner)
@@ -18013,8 +18012,7 @@ Ultimate epistemic interests:
               (print-list (order (mapcar #'inference-number *relevant-nodes*) #'<) 40))))
         (terpri)
         (when *log-on* (terpri) (display-reasoning) (display-queries))
-        (princ ")") (terpri)
-        (setf *print-pretty* pp)))))
+        (princ ")") (terpri)))))
 
 (defun SO (n &optional r) (simulate-oscar n r))
 
@@ -27231,182 +27229,150 @@ be alive.  Should I conclude that Jones becomes dead?"
 (print (formula-code (reform "(all x)((some z)(Q (g x) y) v ((some y)(~(H x y z) & (Q (g x) z)) v (J a b)))")))
 (print (reason-code (reform "(all x)((some z)(Q (g x) y) v ((some y)(~(H x y z) & (Q (g x) z)) v (J a b)))") '(H)))
 
-;;TODO FAILING when turning all of these on
-;;(trace-on)
-;;(display-on)
-;;(proof-on)
-;;(logic-on)
-;;(reductio-on)
-;;(log-on)
-;;(IQ-on)
-;;(graph-log-on)
-;;(setf *j-trace* t)
-;;(setf *s-trace* t)
-;;(setf *safe-trace* t)
+(trace-on)
+(display-on)
+(proof-on)
+(logic-on)
+(reductio-on)
+(log-on)
+(IQ-on)
+(graph-log-on)
+(setf *j-trace* t)
+(setf *s-trace* t)
+(setf *safe-trace* t)
 
-;;;;(test 757) ; the buggy problem
+;;;;(test 757) ; TODO the buggy problem
 
-;;(test)
+(test)
 
-;;(p-test)
+(p-test)
 
-;;TODO FAILING running (so 1) causes (test 757) to give different results
-;;;;(so 1)
-;;;;(so 2)
-;;;;(so 3)
-;;;;(so 4)
-;;;;(so 5)
-;;;;(so 6)
-;;;;(so 7)
-;;;;(so 8)
-;;;;(so 9)
-;;;;(so 10)
-;;;;(so 11)
-;;;;(so 12)
-;;;;(so 13)
-;;;;(so 14)
-;;;;
+(so 1)
+(so 2)
+(so 3)
+(so 4)
+(so 5)
+(so 6)
+(so 7)
+(so 8)
+(so 9)
+(so 10)
+(so 11)
+(so 12)
+(so 13)
+(so 14)
 
-;;;;(find-expectable-values
-;;;; :args '((a = .5) (b = .5) (c = .5) (d = .5) (bc = .25) (bd = .25) (r = .9) (v = .9) (s = .9))
-;;;; :subsets '(A B C D)
-;;;; :probability-constraints '((prob(A / B) = r)
-;;;;                            (prob(A / (B & C)) = s)
-;;;;                            (prob(A / (B & D)) = v))
-;;;; :probability-queries '(prob(A / C)
-;;;;                        prob(A / (B & (C & D))))
-;;;; :independence-queries '(((B & C) (B & D) (A & B)) ((B & C) (B & D) (~A & B))))
-;;;;
-;;;;(find-expectable-values
-;;;; :args '((a = 0.37) (b = 0.42) (ab = 0.16) (c = 0.55) (d = 0.53) (r1 = 0.6) (r2 = 0.55) (s1 = 0.45) (s2 = 0.48))
-;;;; :subsets '(A B C D)
-;;;; :constants '(a b c d ab r1 s1 r2 s2)
-;;;; :probability-constraints '((prob(A / C) = r1)
-;;;;                            (prob(A / D) = s1)
-;;;;                            (prob(B / C) = r2)
-;;;;                            (prob(B / D) = s2))
-;;;; :probability-queries '(prob(A / (C & D))
-;;;;                        prob(B / (C & D)))
-;;;; :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
-;;;;
-;;;;(analyze-probability-structure
-;;;; :name "my-ps"
-;;;; :subsets '(A B C D)
-;;;; :constants '(a b c d ab r1 s1 r2 s2)
-;;;; :probability-constraints '((prob(A / C) = r1)
-;;;;                            (prob(A / D) = s1)
-;;;;                            (prob(B / C) = r2)
-;;;;                            (prob(B / D) = s2))
-;;;; :probability-queries '(prob(A / (C & D))
-;;;;                        prob(B / (C & D)))
-;;;; :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
-;;;;
-;;;;;;TODO FAILING (does not halt)
-;;;;;;(find-expectable-values
-;;;;;; :ps my-ps
-;;;;;; :args '((a = 0.37) (b = 0.42) (ab = 0.16) (c = 0.55) (d = 0.53) (r1 = 0.6) (r2 = 0.55) (s1 = 0.45) (s2 = 0.48))
-;;;;;; :subsets '(A B C D)
-;;;;;; :constants '(a b c d ab r1 s1 r2 s2)
-;;;;;; :probability-constraints '((prob(A / C) = r1)
-;;;;;;                            (prob(A / D) = s1)
-;;;;;;                            (prob(B / C) = r2)
-;;;;;;                            (prob(B / D) = s2))
-;;;;;; :probability-queries '(prob(A / (C & D))
-;;;;;;                        prob(B / (C & D)))
-;;;;;; :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
-;;;;
-;;;;(proclaim '(special Y-ps autoY-ps))
-;;;;
-;;;;(build-probability-structure
-;;;; :name "Y-ps"
-;;;; :subsets '(A B P)
-;;;; :constants '(r s)
-;;;; :probability-constraints '((prob(P / A) = r)
-;;;;                            (prob(P / B) = s))
-;;;; :term-definitions '((abp / (* r a s b) p)
-;;;;                     (ab / (* (- (+ p (* r s)) (+ (* s p) (* r p))) a b) (* p (- 1 p)))
-;;;;                     (ap * r a)
-;;;;                     (bp * s b))
-;;;; :term-characterizations '((a (* (expt (- 1 r) (- 1 r)) (expt r r) a
-;;;;                               (expt (- (+ (* r a) 1) (+ a p)) (- r 1)) (expt (- p (* r a)) (- r))))
-;;;;                           (b (* (expt (- 1 s) (- 1 s)) (expt s s) b
-;;;;                               (expt (- (+ (* s b) 1) (+ b p)) (- s 1)) (expt (- p (* s b)) (- s))))
-;;;;                           (p (/ (* (- p (* s b)) (- p (* r a)) (- 1 p))
-;;;;                               (* (- (+ 1 (* r a)) (+ a p)) (- (+ 1 (* s b)) (+ b p)) p))))
-;;;; :sample-args '((r = .5) (s = .5) (a .5) (b .5) (p .5)))
-;;;;
-;;;;;;TODO FAILING (does not halt)
-;;;;;;(find-expectable-values
-;;;;;; :args '((r = .95) (s = .85) (a .250) (b .250) (p .9))
-;;;;;; :ps Y-ps
-;;;;;; :probability-queries '(prob(P / (A & B))
-;;;;;;                        prob(P / U)
-;;;;;;                        prob(A / U)
-;;;;;;                        prob(B / U)
-;;;;;;                        prob(B / P)
-;;;;;;                        prob(A / P)
-;;;;;;                        prob((A & B) / P)
-;;;;;;                        prob(A / (U & ~P))
-;;;;;;                        prob(B / (U & ~P))
-;;;;;;                        prob((A & B) / (U & ~P)))
-;;;;;; :display t)
-;;;;
-;;;;(build-probability-structure-automatically
-;;;; :name "autoY-ps"
-;;;; :subsets '(A B P)
-;;;; :constants '(r s)
-;;;; :probability-constraints '((prob(P / A) = r)
-;;;;                            (prob(P / B) = s))
-;;;; :sample-args '((r = .5) (s = .5) (a .5) (b .5) (p .5) (ab .25) (ap .25) (bp .25) (abp .125)))
-;;;;
-;;;;;;TODO FAILING (ABS NIL)
-;;;;;;(find-expectable-values
-;;;;;; :args '((r = .95) (s = .85) (a .5) (b .5) (p .5) (ab .25) (ap .25) (bp .25) (abp .125))
-;;;;;; :ps autoY-ps
-;;;;;; :probability-queries '(prob(P / (A & B))
-;;;;;;                        prob(P / U)
-;;;;;;                        prob(A / U)
-;;;;;;                        prob(B / U)
-;;;;;;                        prob(B / P)
-;;;;;;                        prob(A / P)
-;;;;;;                        prob((A & B) / P)
-;;;;;;                        prob(A / (U & ~P))
-;;;;;;                        prob(B / (U & ~P))
-;;;;;;                        prob((A & B) / (U & ~P)))
-;;;;;; :display nil)
-;;;;
-;;;;;;TODO
-;;;;;;(tabulate-expectable-values #'(lambda (x) (let ((y (assoc 'p x))) (or (mem3 y) (mem2 y)))) Y-ps 'r 's)
-;;;;
-;;;;;;TODO
-;;;;;;(tabulate-expectable-values #'(lambda (x) (let ((y (assoc 'p x))) (or (mem3 y) (mem2 y)))) autoY-ps 'r 's)
-;;
-;;(let ((*problems*
-;;        (make-problem-list "
-;;Problem #758
-;;Given premises:
-;;        (all A)(all B)((subset A B) <-> (all x) ((mem x A) -> (mem x B)))                                    justification = 1.0
-;;        (all A)(all B)(all x)((mem x (int A B)) <-> ((mem x A) & (mem x B)))                                 justification = 1.0
-;;        (all A)(all B)((maps F A B) <->
-;;                        ((all x)((mem x A) -> (some y)((mem y B) & (F x y)))
-;;                        & (all x)(all y)(all z)(((mem x A) & ((mem y A) & (mem z A))) -> (((F x y) & (F x z)) -> (y = z)))))
-;;                                                                                                             justification = 1.0
-;;        (all A)(all x)(all y)((x = y) -> ((mem x A) -> (mem y A)))                                           justification = 1.0
-;;        (all A)(all B)((equal A B) <-> ((subset A B) & (subset B A)))                                        justification = 1.0
-;;        (all B)(all A)(all B)(all x)((mem x (inv F B A)) <-> ((mem x A) & (some y)((mem y B) & (F x y))))    justification = 1.0
-;;Ultimate epistemic interests:
-;;        (all A)(all B)(all X)(all Y)(((maps F A B) & ((subset X B) & (subset Y B)))
-;;                                                                 ->
-;;        (equal (inv F (int X Y) A) (int (inv F X A) (inv F Y A))))                                           interest = 1.0
-;;        (all A)(all B)(all X)(all Y)(((maps F A B) & ((subset X B) & (subset Y B)))
-;;                                                                 ->
-;;        (equal (inv F (int X Y) A) (int (inv F X A) (inv F Y A))))                                           interest = 1.0
-;;        (all A)(all B)(all X)(all Y)(((maps F A B) & ((subset X B) & (subset Y B)))
-;;                                                                 ->
-;;        (equal (inv F (int X Y) A) (int (inv F X A) (inv F Y A))))                                           interest = 1.0
-;;        (all A)(all B)(all X)(all Y)(((maps F A B) & ((subset X B) & (subset Y B)))
-;;                                                                 ->
-;;        (equal (inv F (int X Y) A) (int (inv F X A) (inv F Y A))))                                           interest = 1.0
-;;")))
-;;  (test))
-;;(test 757)
+
+(find-expectable-values
+ :args '((a = .5) (b = .5) (c = .5) (d = .5) (bc = .25) (bd = .25) (r = .9) (v = .9) (s = .9))
+ :subsets '(A B C D)
+ :probability-constraints '((prob(A / B) = r)
+                            (prob(A / (B & C)) = s)
+                            (prob(A / (B & D)) = v))
+ :probability-queries '(prob(A / C)
+                        prob(A / (B & (C & D))))
+ :independence-queries '(((B & C) (B & D) (A & B)) ((B & C) (B & D) (~A & B))))
+
+(find-expectable-values
+ :args '((a = 0.37) (b = 0.42) (ab = 0.16) (c = 0.55) (d = 0.53) (r1 = 0.6) (r2 = 0.55) (s1 = 0.45) (s2 = 0.48))
+ :subsets '(A B C D)
+ :constants '(a b c d ab r1 s1 r2 s2)
+ :probability-constraints '((prob(A / C) = r1)
+                            (prob(A / D) = s1)
+                            (prob(B / C) = r2)
+                            (prob(B / D) = s2))
+ :probability-queries '(prob(A / (C & D))
+                        prob(B / (C & D)))
+ :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
+
+(analyze-probability-structure
+ :name "my-ps"
+ :subsets '(A B C D)
+ :constants '(a b c d ab r1 s1 r2 s2)
+ :probability-constraints '((prob(A / C) = r1)
+                            (prob(A / D) = s1)
+                            (prob(B / C) = r2)
+                            (prob(B / D) = s2))
+ :probability-queries '(prob(A / (C & D))
+                        prob(B / (C & D)))
+ :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
+
+;;TODO FAILING (does not halt)
+;;(find-expectable-values
+;; :ps my-ps
+;; :args '((a = 0.37) (b = 0.42) (ab = 0.16) (c = 0.55) (d = 0.53) (r1 = 0.6) (r2 = 0.55) (s1 = 0.45) (s2 = 0.48))
+;; :subsets '(A B C D)
+;; :constants '(a b c d ab r1 s1 r2 s2)
+;; :probability-constraints '((prob(A / C) = r1)
+;;                            (prob(A / D) = s1)
+;;                            (prob(B / C) = r2)
+;;                            (prob(B / D) = s2))
+;; :probability-queries '(prob(A / (C & D))
+;;                        prob(B / (C & D)))
+;; :independence-queries '((C D A) (C D (U & ~A)) (C D B) (C D (U & ~B))))
+
+(proclaim '(special Y-ps autoY-ps))
+
+(build-probability-structure
+ :name "Y-ps"
+ :subsets '(A B P)
+ :constants '(r s)
+ :probability-constraints '((prob(P / A) = r)
+                            (prob(P / B) = s))
+ :term-definitions '((abp / (* r a s b) p)
+                     (ab / (* (- (+ p (* r s)) (+ (* s p) (* r p))) a b) (* p (- 1 p)))
+                     (ap * r a)
+                     (bp * s b))
+ :term-characterizations '((a (* (expt (- 1 r) (- 1 r)) (expt r r) a
+                               (expt (- (+ (* r a) 1) (+ a p)) (- r 1)) (expt (- p (* r a)) (- r))))
+                           (b (* (expt (- 1 s) (- 1 s)) (expt s s) b
+                               (expt (- (+ (* s b) 1) (+ b p)) (- s 1)) (expt (- p (* s b)) (- s))))
+                           (p (/ (* (- p (* s b)) (- p (* r a)) (- 1 p))
+                               (* (- (+ 1 (* r a)) (+ a p)) (- (+ 1 (* s b)) (+ b p)) p))))
+ :sample-args '((r = .5) (s = .5) (a .5) (b .5) (p .5)))
+
+;;TODO FAILING (does not halt)
+;;(find-expectable-values
+;; :args '((r = .95) (s = .85) (a .250) (b .250) (p .9))
+;; :ps Y-ps
+;; :probability-queries '(prob(P / (A & B))
+;;                        prob(P / U)
+;;                        prob(A / U)
+;;                        prob(B / U)
+;;                        prob(B / P)
+;;                        prob(A / P)
+;;                        prob((A & B) / P)
+;;                        prob(A / (U & ~P))
+;;                        prob(B / (U & ~P))
+;;                        prob((A & B) / (U & ~P)))
+;; :display t)
+
+(build-probability-structure-automatically
+ :name "autoY-ps"
+ :subsets '(A B P)
+ :constants '(r s)
+ :probability-constraints '((prob(P / A) = r)
+                            (prob(P / B) = s))
+ :sample-args '((r = .5) (s = .5) (a .5) (b .5) (p .5) (ab .25) (ap .25) (bp .25) (abp .125)))
+
+;;TODO FAILING (ABS NIL)
+;;(find-expectable-values
+;; :args '((r = .95) (s = .85) (a .5) (b .5) (p .5) (ab .25) (ap .25) (bp .25) (abp .125))
+;; :ps autoY-ps
+;; :probability-queries '(prob(P / (A & B))
+;;                        prob(P / U)
+;;                        prob(A / U)
+;;                        prob(B / U)
+;;                        prob(B / P)
+;;                        prob(A / P)
+;;                        prob((A & B) / P)
+;;                        prob(A / (U & ~P))
+;;                        prob(B / (U & ~P))
+;;                        prob((A & B) / (U & ~P)))
+;; :display nil)
+
+;;TODO
+;;(tabulate-expectable-values #'(lambda (x) (let ((y (assoc 'p x))) (or (mem3 y) (mem2 y)))) Y-ps 'r 's)
+
+;;TODO
+;;(tabulate-expectable-values #'(lambda (x) (let ((y (assoc 'p x))) (or (mem3 y) (mem2 y)))) autoY-ps 'r 's)
