572ea151275fa43d3be1eb2a3a61493c2588d0df
Martin Stone Davis <martin.stone.davis@gmail.com>
2016-11-16 23:59:59 -0800

    add run-and-compare.sh; add run-all-tests; slightly simplify notational-variants

--- lisp-sbcl/run-and-compare.sh
+++ lisp-sbcl/run-and-compare.sh
@@ -1,1 +1,4 @@
 sbcl --noinform --load monolithic-oscar.lisp --eval "(run-all-tests)" --non-interactive 2>&1 | tee run.log > /dev/null
+gunzip run.log.saved.gz
+diff run.log run.log.saved | less -F
+gzip run.log.saved
