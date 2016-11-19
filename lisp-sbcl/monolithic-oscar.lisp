95b5555081095d7ce74106bd51db997f1eb79f10
Martin Stone Davis <martin.stone.davis@gmail.com>
2016-11-18 16:20:02 -0800

    add make-hypernode*

--- lisp-sbcl/monolithic-oscar.lisp
+++ lisp-sbcl/monolithic-oscar.lisp
@@ -387,7 +387,17 @@
 
 (defvar *suppress-volatile-display-p* t)
 
+(defvar *log-p* t)
+
                                         ;                                                           *MACROS*
+
+(defmacro make-hypernode* (&rest args)
+  "make-hypernode, not needing a hypernode-number argument, and possibly logging"
+  `(progn
+     (let ((hypernode (make-hypernode :hypernode-number (incf *hypernode-number*) ,@args)))
+       ,(when *log-p*
+          `(format *standard-output* "MAKING HYPERNODE #~A (~A)~%" *hypernode-number* hypernode))
+       hypernode)))
 
 (defmacro find-if! (&rest args)
   "find-if, asserting that the item is unique if found"
