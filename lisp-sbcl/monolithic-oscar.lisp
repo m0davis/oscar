249e300b6bcc1c8aa482ad0c04585245d816a917
Martin Stone Davis <martin.stone.davis@gmail.com>
2016-11-23 06:19:31 -0800

    update problem 758

--- lisp-sbcl/monolithic-oscar.lisp
+++ lisp-sbcl/monolithic-oscar.lisp
@@ -371,11 +371,11 @@
 
 (setf *simulation-problems* nil)
 
-(defvar *test-assertions-p* t)
+(defvar *test-assertions-p* nil)
 
 (defvar *suppress-volatile-display-p* t)
 
-(defparameter *log-p* t)
+(defparameter *log-p* nil)
 
 (defvar *log-retrievals-p* t)
                                         ;                                                           *MACROS*
@@ -15212,40 +15212,60 @@
 (defun default-problem-list ()
   (make-problem-list
    "
-;Problem #758
-;same as problem 757 but rewritten with only expressions of first-order logic TODO
-;Given premises:
-;        (all x)(all A)((mem x A) -> (set A))                                                                 justification = 1.0
-;
-;        (all A)(all B)((subset A B) -> ((set A) & (set B))                                                   justification = 1.0
-;        (all A)(all B)((subset A B) <-> (all x)((mem x A) -> (mem x B)))                                     justification = 1.0
-;
-;        (all A)(all B)(all intAB)((IsInt A B intAB) ->
-;        (all A)(all B)(all intAB)((IsInt A B intAB) <-> (all x)((mem x intAB) <-> ((mem x A) & (mem x B))))  justification = 1.0
-;        (all A)(all B)((mapsF A B) <->
-;                        ((all x)((mem x A) -> (some y)((mem y B) & (F x y)))
-;                        & (all x)(all y)(all z)(((mem x A) & ((mem y A) & (mem z A))) -> (((F x y) & (F x z)) -> (same y z)))))
-;                                                                                                             justification = 1.0
-;        (all A)(all x)(all y)((same x y) -> ((mem x A) -> (mem y A)))                                        justification = 1.0
-;        (all x)(same x x)                                                                                    justification = 1.0
-;        (all x)(all y)((same x y) <-> (same y x))                                                            justification = 1.0
-;        (all x)(all y)(all z)(((same x y) & (same y z)) -> (same x z))                                       justification = 1.0
-;        (all A)(all B)((equal A B) <-> ((subset A B) & (subset B A)))                                        justification = 1.0
-;        (all A)(all B)(all invFBA)((IsInvF B A invFBA) <-> (all x)((mem x invFBA) <-> ((mem x A) & (some y)((mem y B) & (F x y)))))
-;                                                                                                             justification = 1.0
-;
-;Ultimate epistemic interests:
-;        (all A)(all B)(all X)(all Y)(
-;          ((mapsF A B) & ((subset X B) & (subset Y B)))
-;          ->
-;          (some intXY)((IsInt X Y intXY) &
-;          (some invFIntXYA)((IsInvF intXY A invFIntXYA) &
-;          (some invFXA)((IsInvF X A invFXA) &
-;          (some invFYA)((IsInvF Y A invFYA) &
-;          (some intInvFXAInvFYA)((IsInt invFXA invFYA intInvFXAInvFYA) &
-;            (equal invFIntXYA intInvFXAInvFYA)
-;          ))))))
-;        interest = 1.0
+Problem #758
+same as problem 757 but rewritten with only expressions of first-order logic TODO may be incorrect, may contain unnecessary premises, this runs and runs making no discernable progress
+Given premises:
+        ; elemental equality
+        (all x)(same x x)                                                                                    justification = 1.0
+        (all x)(all y)((same x y) <-> (same y x))                                                            justification = 1.0
+        (all x)(all y)(all z)(((same x y) & (same y z)) -> (same x z))                                       justification = 1.0
+
+        ; membership
+        (all A)((set A) -> (all x)(all y)((same x y) -> ((mem x A) <-> (mem y A))))                          justification = 1.0
+
+        ; subsetting
+        (all A)(all B)(((set A) & (set B)) -> ((subset A B) <-> (all x)((mem x A) -> (mem x B))))            justification = 1.0
+
+        ; intersection
+        (all A)(all B)(((set A) & (set B)) -> (some intAB)((set intAB) & (IsInt A B intAB)))                 justification = 1.0
+        (all A)(all B)(all intAB)((((set A) & (set B)) & (set intAB)) -> ((IsInt A B intAB) <-> (all x)((mem x intAB) <-> ((mem x A) & (mem x B)))))
+                                                                                                             justification = 1.0
+
+        ; mapping
+        (all A)(all B)(((set A) & (set B)) ->
+                        ((mapsF A B) <->
+                          ((all x)((mem x A) -> (some y)((mem y B) & (F x y)))
+                           &
+                           (all x)(all y)(all z)(((mem x A) & ((mem y A) & (mem z A))) -> (((F x y) & (F x z)) -> (same y z))))))
+                                                                                                             justification = 1.0
+        (all x)(all y)(all z)((same y z) -> ((F x y) <-> (F x z)))                                           justification = 1.0
+
+        ; subset equality
+        (all A)(all B)(((set A) & (set B)) -> (equal A B) <-> ((subset A B) & (subset B A)))                 justification = 1.0
+
+        ; inversion
+        (all A)(all B)(((set A) & (set B)) -> (some invFBA)((set invFBA) & (IsInvF B A invFBA)))             justification = 1.0
+        (all A)(all B)(all invFBA)((((set A) & (set B)) & (set invFBA)) -> ((IsInvF B A invFBA) <-> (all x)((mem x invFBA) <-> ((mem x A) & (some y)((mem y B) & (F x y))))))
+                                                                                                             justification = 1.0
+
+Ultimate epistemic interests:
+        (all A)((set A) ->
+        (all B)((set B) ->
+        (all X)((set X) ->
+        (all Y)((set Y) ->
+          (
+            ((mapsF A B) & ((subset X B) & (subset Y B)))
+            ->
+            (some intXY)(((set intXY) & (IsInt X Y intXY)) &
+            (some invFIntXYA)(((set invFIntXYA) & (IsInvF intXY A invFIntXYA)) &
+            (some invFXA)(((set invFXA) & (IsInvF X A invFXA)) &
+            (some invFYA)(((set invFYA) & (IsInvF Y A invFYA)) &
+            (some intInvFXAInvFYA)(((set intInvFXAInvFYA) & (IsInt invFXA invFYA intInvFXAInvFYA)) &
+              (equal invFIntXYA intInvFXAInvFYA)
+            )))))
+          )
+        ))))
+        interest = 1.0
 
 Problem #757
 Given premises:
